b6c10a6fb05c450a2e808779cffa052dcae9349e
TaiNguyen <tai@useget.com>
2021-02-02 22:35:05 +0700

    add navigation

--- App.js
+++ App.js
@@ -7,56 +7,27 @@
  */
 
 import React from 'react';
-import Feather from 'react-native-vector-icons/Feather';
-import {View} from 'react-native';
-import Svg, {
-  Circle,
-  Ellipse,
-  G,
-  Text,
-  TSpan,
-  TextPath,
-  Path,
-  Polygon,
-  Polyline,
-  Line,
-  Rect,
-  Use,
-  Image,
-  Symbol,
-  Defs,
-  LinearGradient,
-  RadialGradient,
-  Stop,
-  ClipPath,
-  Pattern,
-  Mask,
-} from 'react-native-svg';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Home = () => {
+  return (
+    <View>
+      <Text>hom</Text>
+    </View>
+  );
+};
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <View>
-      <Feather name="home" />
-      <Svg height="50%" width="50%" viewBox="0 0 100 100">
-        <Circle
-          cx="50"
-          cy="50"
-          r="45"
-          stroke="blue"
-          strokeWidth="2.5"
-          fill="green"
-        />
-        <Rect
-          x="15"
-          y="15"
-          width="70"
-          height="70"
-          stroke="red"
-          strokeWidth="2"
-          fill="yellow"
-        />
-      </Svg>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
